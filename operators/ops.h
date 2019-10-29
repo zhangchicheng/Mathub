ccdb214e16cb7f2c03e33a7ca79387342e3c2e53
Chicheng <zhang.chic@husky.neu.edu>
2019-10-28 22:43:23 -0400

    Update ops.h

--- operators/ops.h
+++ operators/ops.h
@@ -5,78 +5,55 @@
 #include<iostream>
 namespace Mathub
 {
-template <typename Op, typename T, typename Arg> struct UnaryOp;
+template <typename Op, typename Arg> struct UnaryOp;
 template <typename Op, typename Lhs, typename Rhs> struct BinaryOp;
 
 template <typename Op, typename T, typename Arg>
-inline UnaryOp<Op, T, Arg> F(const Arg& arg);
+inline UnaryOp<Op, Arg> F(const Arg& arg);
+
 template <typename Op, typename T, typename Lhs, typename Rhs>
 inline BinaryOp<Op, Lhs, Rhs> F(const Lhs& lhs, const Rhs& rhs);
 
-template <typename Lhs, typename Rhs>
-struct plus
-{
-    //inline static auto map(T a) { return a; }
-    inline static auto map(Lhs a, Rhs b) { return a + b; }
-};
+struct plus {};
+struct minus {};
+struct mul {};
+struct div {};
 
-template <typename T>
-struct minus
+template <typename Arg>
+inline UnaryOp<plus, Arg> operator+(const Arg& arg)
 {
-    inline static auto map(T a) { return -a; }
-    inline static auto map(T a, T b) { return a - b; }
-};
-
-template <typename T>
-struct mul
-{
-    inline static auto map(T a, T b) { return a * b; }
-};
-
-template <typename T>
-struct div
-{
-    inline static auto map(T a, T b) { return a / b; }
-};
-
-/*
-template <typename Arg, typename T = typename Arg::DataType>
-inline UnaryOp<plus<T>, T, Arg> operator+(const Arg& arg)
-{
-    return F<plus<T>, T>(arg);
+    return F<plus>(arg);
 }
- */
 
-template <typename Arg, typename T = typename Arg::DataType>
-inline UnaryOp<minus<T>, T, Arg> operator-(const Arg& arg)
+template <typename Arg>
+inline UnaryOp<minus, Arg> operator-(const Arg& arg)
 {
-    return F<minus<T>, T>(arg);
+    return F<minus>(arg);
 }
 
 template <typename Lhs, typename Rhs>
-inline BinaryOp<plus<Lhs, Rhs>, Lhs, Rhs> operator+(const Lhs& lhs, const Rhs& rhs)
+inline BinaryOp<plus, Lhs, Rhs> operator+(const Lhs& lhs, const Rhs& rhs)
 {
-    return F<plus<Lhs, Rhs>>(lhs, rhs);
-}
-/*
-template <typename Lhs, typename Rhs, typename T = typename Lhs::SubType>
-inline BinaryOp<minus<T>, Lhs, Rhs> operator-(const Lhs& lhs, const Rhs& rhs)
-{
-    return F<minus<T>>(lhs, rhs);
+    return F<plus>(lhs, rhs);
 }
 
-template <typename Lhs, typename Rhs, typename T = typename Promote<typename Lhs::DataType, typename Rhs::DataType>::type>
-inline BinaryOp<mul<T>, Lhs, Rhs> operator*(const Lhs& lhs, const Rhs& rhs)
+template <typename Lhs, typename Rhs>
+inline BinaryOp<minus, Lhs, Rhs> operator-(const Lhs& lhs, const Rhs& rhs)
 {
-    return F<mul<T>, T>(lhs, rhs);
+    return F<minus>(lhs, rhs);
 }
 
-template <typename Lhs, typename Rhs, typename T = typename Promote<typename Lhs::DataType, typename Rhs::DataType>::type>
-inline BinaryOp<div<T>, Lhs, Rhs> operator/(const Lhs& lhs, const Rhs& rhs)
+template <typename Lhs, typename Rhs>
+inline BinaryOp<mul, Lhs, Rhs> operator*(const Lhs& lhs, const Rhs& rhs)
 {
-    return F<div<T>, T>(lhs, rhs);
+    return F<mul>(lhs, rhs);
 }
- */
+
+template <typename Lhs, typename Rhs>
+inline BinaryOp<div, Lhs, Rhs> operator/(const Lhs& lhs, const Rhs& rhs)
+{
+    return F<div>(lhs, rhs);
+}
 
 }
 
