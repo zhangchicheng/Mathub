0e82dcaf476b40bdcc73a2cebd2989643e4ae350
Chicheng <zhang.chic@husky.neu.edu>
2019-11-04 19:14:24 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,39 +1,28 @@
 #include "mathub.h"
-#include<iostream>
 
 using namespace Mathub;
 
 int main()
 {
-    Array<int, 2,1,2> m0 = {1,2,3,4};
-    std::cout << m0 << std::endl;
+    Vector<int,3> vec = {2,4,6}; // or Array<int,3>
+    std::cout << vec << std::endl;
 
-    Tensor<int, 2,3,2> m1 = {1,3,5,7,9,2,4,6,8,1,2,4};
-    std::cout << m1[1][1][0] << std::endl;
-    std::cout << m1[1][0].dim << std::endl;
-    std::cout << m1.shape[2] << std::endl;
-    std::cout << m1 << std::endl;
+    Matrix<int,2,2> mat = {1,3,5,7}; // or Array<int,2,2>
+    Matrix<int,2,1> mat2 = {2,3};
+    Matrix<int,2,2> mat3;
+    mat3 = -mat + mat2 * mat2;
+    std::cout << mat3 << std::endl;
 
-    Matrix<int,2,1> m2 = {2,3};
-    std::cout << m2 << std::endl;
+    Tensor<int,2,3,2> arr = {0,1,2,3,4,5,6,7,8,9,0,1}; // or Array<int,2,3,2>
+    std::cout << arr << std::endl;
+    std::cout << arr(1,1,0) << std::endl;
+    std::cout << arr[1][1][0] << std::endl;
+    std::cout << arr[1][0].dim << std::endl;
+    std::cout << arr.shape[2] << std::endl;
 
-    std::cout<<m1(1,0,0)<<std::endl;
-
-    Matrix <int,1,3> v4 = {1,2,3};
-    Matrix <int,1,3> v5 = {2,1,2};
-    Matrix <int,1,3> v6 = {0,0,0};
-    v6 = v4+v5;
-    std::cout<<v6[0]<<std::endl;
-    Matrix<int, 2,3> v0 = {1,2,3,2,1,2};
-    Matrix<int, 2,3> v1 = {0,1,4,2,2,4};
-    Matrix<int, 2,3> v2 = {0,0,0,0,0,0};
-    v2 = v0+v0*v1;
-    std::cout<<v2[0]<<std::endl;
-
-    Tensor<int, 2,2,3> t0 = {1,2,3,2,1,2,3,4,5,7,8,9};
-    Tensor<int, 2,1,3> t1 = {0,1,4,1,2,3};
-    //Tensor<int, 2,2,3> t3 = {0,0,0,0,0,0,0,0,0,0,0,0};
-    Tensor<int, 2,2,3> t3;
-    t3 = t0+t1;
-    std::cout<<t3<<std::endl;
+    Tensor<int, 2,2,3> arr1 = {1,2,3,2,1,2,3,4,5,7,8,9};
+    Tensor<int, 2,1,3> arr2 = {0,1,4,1,2,3};
+    Tensor<int, 2,2,3> arr3 = {0,0,0,0,0,0,0,0,0,0,0,0};
+    arr3 = arr1+arr2;
+    std::cout<<arr3<<std::endl;
 }
