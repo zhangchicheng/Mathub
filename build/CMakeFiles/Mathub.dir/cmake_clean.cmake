3d91e41189dedbf9ca580cbeb58063c1a8930766
zhangc <zhangc@fortinet.com>
2025-06-11 11:13:41 -0700

    delete unused dir

---
+++ build/CMakeFiles/Mathub.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mathub.dir/example.cpp.o"
+  "CMakeFiles/Mathub.dir/example.cpp.o.d"
+  "Mathub"
+  "Mathub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mathub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- mathub.h
+++
@@ -1,7 +0,0 @@
-#ifndef MATHUB_H
-#define MATHUB_H
-
-#include "./blob/core.h"
-#include "./expression/operations.h"
-
-#endif
